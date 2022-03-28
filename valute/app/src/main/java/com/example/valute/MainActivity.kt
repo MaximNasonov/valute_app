3e3a6534dc9020ab37535caf7055f66b99ae43bd
MaximNasonov <101695300+MaximNasonov@users.noreply.github.com>
2022-03-28 11:16:46 +0700

    Add v.2
    Add converter function
    Update interface

--- valute/app/src/main/java/com/example/valute/MainActivity.kt
+++ valute/app/src/main/java/com/example/valute/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.valute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.valute.databinding.ActivityMainBinding
 import com.example.valute.tmp.Valutes
@@ -20,6 +23,8 @@ class MainActivity (
 ) : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private val adapter = Adapter()
+    private var count = 0.0
+    private var number = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,28 +39,57 @@ class MainActivity (
         val result = future.get(5, TimeUnit.SECONDS)
         val maptype = object:TypeToken<Map<String,Any>>(){}.type
         val maper = Gson().fromJson<Map<String,Any>>(result, maptype)
-        val qwer = maper.get("Valute") as Map<String,Any>
-        val qwert = qwer.map {
-            ValuteName(it.value as Map<String,Any>)
+        val listValute = maper.get("Valute") as Map<String,Any>
+        val listNameWithNominal = listValute.map {
+            createValuteNameWithNominal(it.value as Map<String,Any>)
         }
-        val qwert3 = qwer.map {
-            ValuteValue(it.value as Map<String,Any>)
+        val listValueWithR = listValute.map {
+            creatValueWithR(it.value as Map<String,Any>)
         }
-        val qwert2 = qwer.map {
-            ValuteCharCode(it.value as Map<String,Any>)
+        val listCharCode = listValute.map {
+            createCharCode(it.value as Map<String,Any>)
         }
-        init(qwert,qwert2,qwert3,qwer.size)
+        val listName = listValute.map {
+            createValuteName(it.value as Map<String,Any>)
+        }
+        val listValueWithNominal = listValute.map {
+            creatValueWithNominal(it.value as Map<String,Any>)
+        }
+        init(listNameWithNominal,listCharCode,listValueWithR,listValute.size)
+        val spiner = binding.spinner
+        val adapter2: ArrayAdapter<String> = ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_item, listName)
+        spiner.adapter = adapter2
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spiner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                count = listValueWithNominal.get(position)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+        binding.button.setOnClickListener {
+            number = binding.value.text.toString().toDouble()
+            binding.result.text = String.format("%.2f", (number/count))
+        }
     }
     override fun onDestroy() {
         executor.shutdown()
         super.onDestroy()
     }
 
-    fun ValuteCharCode(map: Map<String,Any>): String {
+    fun createCharCode(map: Map<String,Any>): String {
         val CharCode = map.get("CharCode").toString()
         return CharCode
     }
-    fun ValuteName(map: Map<String,Any>): String {
+    fun createValuteNameWithNominal(map: Map<String,Any>): String {
         var Name= map.get("Name").toString()
         val Nominal= map.get("Nominal") as Double
         if (Nominal>1) {
@@ -64,24 +98,34 @@ class MainActivity (
         }
         return "$Name"
     }
-    fun ValuteValue(map: Map<String,Any>): String {
+    fun createValuteName(map: Map<String,Any>): String {
+        var name= map.get("Name").toString()
+        return name
+    }
+    fun creatValueWithR(map: Map<String,Any>): String {
+        var Value= map.get("Value") as Double
+        return String.format("%.4f", Value) + " ₽"
+    }
+    fun creatValueWithNominal(map: Map<String,Any>): Double {
         var Value= map.get("Value") as Double
         val Nominal= map.get("Nominal") as Double
-        return String.format("%.4f", Value) + " ₽"
+            Value/=Nominal
+        return Value
     }
     private fun init(name:List<String>,value:List<String>,valute:List<String>,size:Int){
         binding.apply {
             rcView.layoutManager = LinearLayoutManager(this@MainActivity)
             rcView.adapter = adapter
-            val a = size
-            for(a in 0..33) {
-                val plant = Valutes(name.get(a), value.get(a), valute.get(a))
-                adapter.addValute(plant)
+            val a = size - 1
+            for(i in 0..a) {
+                val item = Valutes(name[i], value[i], valute[i])
+                adapter.addValute(item)
             }
 
         }
     }
 }
+
 
 class Valute(
     val CharCode: String,
